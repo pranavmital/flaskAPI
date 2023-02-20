572fcfeba6c09a515d51d04d07a38f1216d2e114
Pranav Mital <pranavmital27@gmail.com>
2023-02-19 21:19:11 -0800

    created a minimal flask application

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import random
+
+app_variable = Flask(__name__)
+
+@app_variable.route("/")
+def hello_world():
+    print("Home path reached!")
+    return "<p>Hello, World!</p>"
+
+@app_variable.route("/a") #custom path
+def path_a():
+    print("Random number path reached!")
+    custom_string = str(random.randint(10,1000))
+    return "<p>Hello, {}</p>".format(custom_string)
